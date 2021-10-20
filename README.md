434c1828a8f15f89f4d27d9208414a18180e1250
BrunoUemura <bruno.h.uemura@outlook.com>
2021-10-20 01:54:52 -0300

    Done stage-1 until 1:03:22

---
+++ README.md
@@ -0,0 +1,23 @@
+# NLW HEAT
+
+## Stage 1
+
+Node.js App
+
+- [x] Create project
+- [x] Install Express, Prisma, Typescript
+- [x] Configure Github OAuth
+- [x] Create login github route
+- [x] Create callback route
+- [x] User authentication receiving the code
+- [x] Message registration
+- [x] Websocket configuration
+- [x] Return last 3 messages
+- [x] Create user profile
+
+### Technologies
+
+- Node.js
+- TypeScript
+- Express
+- Prisma
---
+++ node/src/services/AuthenticateUserService.ts
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+/**
+ * Receive code(string)
+ * Retrieve the access_token from github
+ * Retrieve user info from github
+ * Check if user exists in DB
+ * YES ===> Generate a token
+ * NO  ===> Create user and generate token
+ * Return the token with user info
+ */
+
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
+interface IUserResponse {
+  avatar_url: string;
+  login: string;
+  id: number;
+  name: string;
+}
+
+class AuthenticateUserService {
+  async execute(code: string) {
+    const url = "https://github.com/login/oauth/access_token";
+    const { data: accessTokenResponse } =
+      await axios.post<IAccessTokenResponse>(url, null, {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+    const { data } = await axios.get<IUserResponse>(
+      "https://api.github.com/user",
+      {
+        headers: {
+          authorization: `Bearer ${accessTokenResponse.access_token}`,
+        },
+      }
+    );
+
+    return data;
+  }
+}
+
+export { AuthenticateUserService };
