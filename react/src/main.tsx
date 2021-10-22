afb6d7e1099957cbd6746c66a3ace397cbdd82bf
BrunoUemura <bruno.h.uemura@outlook.com>
2021-10-22 19:16:10 -0300

    Added elixt and react-native

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { App } from "./App";
 import { AuthProvider } from "./context/auth";
 
-import "./styles/global.css";
+import "./styles/global.scss";
 
 ReactDOM.render(
   <React.StrictMode>
